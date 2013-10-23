17ebe319cb846c1cbba2086139b7339b8263c4a5
Kelvin Lam <kelvinl@sakurasky.com>
2013-10-23 11:57:44 +1100

    Refactored argparse to make it easier to do arguments checking.

--- src/cantonhk.py
+++ src/cantonhk.py
@@ -12,6 +12,29 @@ from models import CantonKey
 SUPPORTED_FORMAT = ['ibus']
 
 
+def deploy(options):
+
+    if not hasattr(options, 'format'):
+        exit('Format not defined')
+    elif not hasattr(options, 'input_path'):
+        exit('IM table not found.')
+
+    if options.format == 'ibus':
+        deploy_ibus(options.input_path)
+
+
+def deploy_ibus(input_path):
+    from subprocess import call
+    pass
+
+
+def read(options):
+    if hasattr(options, 'input_file'):
+        read_donar_table(options.input_file)
+    else:
+        exit('Missing input file.')
+
+
 def read_donar_table(input_file):
     print "input file = %s" % input_file
 
@@ -33,48 +56,46 @@ def read_donar_table(input_file):
     return keymap
 
 
-def generate_table(format, input_path, output_path):
+def write(options):
+    if not hasattr(options, 'format'):
+        exit("Output format not defined.")
+    elif options.format not in SUPPORTED_FORMAT:
+        exit("Format %s is'nt supported yet." % options.format)
+    elif not hasattr(options, 'input_file'):
+        exit("Missing input file")
+    elif not hasattr(options, 'output_path'):
+        exit("Missing output path")
+    else:
+        generate_table(options.format,
+                       input_path=options.input_file,
+                       output_path=options.output_path)
+
+
+def generate_table(im_format, input_path, output_path):
     keymap = read_donar_table(input_path)
 
     django_settings.configure()
 
-    t = Template(codecs.open('/home/kuro/python/cantonhk/res/templates/ibus.txt', 'r', encoding='utf-8').read())
-
-    with codecs.open(output_path, mode='w', encoding='utf-8') as f:
-        print >>f, t.render(Context({'keys': keymap}))
+    if im_format == 'ibus':
+        t = Template(codecs.open('/home/kuro/python/cantonhk/res/templates/ibus.txt', 'r', encoding='utf-8').read())
+        with codecs.open(output_path, mode='w', encoding='utf-8') as f:
+            print >>f, t.render(Context({'keys': keymap}))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
-    #parser.add_argument(dest="action", choices=['read', 'write'], help="Action applied to file.")
-    #parser.add_argument("-f", "--format", dest="format", choices=['mac', 'gcin', 'ibus'],
-    #                    help="The output format for target IM daemon.")
 
-
-    #create the parser for the 'download' command
     read_parser = subparsers.add_parser('read', help='read help')
     read_parser.add_argument(dest='input_file', help='File to read in.')
+    read_parser.set_defaults(func=read)
 
     write_parser = subparsers.add_parser('write', help='write help')
     write_parser.add_argument("-i", "--input", dest='input_file', help='File to read in.')
     write_parser.add_argument("-f", "--format", dest="format", choices=['mac', 'gcin', 'ibus'],
                               help="The output format for target IM daemon.")
-    write_parser.add_argument("-o", "--output", dest='output_path', help='Output path for generated table')
+    write_parser.add_argument("-o", "--output", dest='output_path',
+                              help='Output path for generated table')
+    write_parser.set_defaults(func=write)
 
     options = parser.parse_args()
-
-    if hasattr(options, 'format'):
-        if options.format not in SUPPORTED_FORMAT:
-            exit("Format %s is'nt supported yet." % options.format)
-        elif not hasattr(options, 'input_file'):
-            exit("Missing input file")
-        elif not hasattr(options, 'output_path'):
-            exit("Missing output path")
-        else:
-            generate_table(options.format,
-                           input_path=options.input_file,
-                           output_path=options.output_path)
-    elif hasattr(options, 'input_file'):
-        read_donar_table(options.input_file)
-    else:
-        exit("I don't know what the fuck you're trying to do here.")
+    options.func(options)
